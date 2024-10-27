da15f04eccd0eff870e2291be9bb7dc201cd909e
Emma McArthur <emmc3846@colorado.edu>
2024-10-27 13:29:26 -0600

    Update Week9.cpp

--- Week9.cpp
+++ Week9.cpp
@@ -3,11 +3,8 @@ using namespace std;
 
 int main() {
     int array[5];
-    for (int i=0; i<=5; i++) {
+    for (int i=0; i<5; i++) {
         cout <<array[i]<<endl;
-    }
-    for (int j=0; j<2; j--) {
-        cout<<j<<endl;
     }
     return 0;
 }

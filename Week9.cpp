eca9560f795962815ea82c2ea434343b066d551a
Emma McArthur <emmc3846@colorado.edu>
2024-10-27 15:22:19 -0600

    Added week9.cpp

---
+++ Week9.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int array[5];
+    for (int i=0; i<=5; i++){
+        cout <<array[i]<<endl;
+    }
+    for (int j=0; j<2;j--) {
+        cout <<array[j]<<endl;
+    }
+}
